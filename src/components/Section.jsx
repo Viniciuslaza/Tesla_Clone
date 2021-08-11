83a60b5c496ce618e5ba404a4bdecd4f2af915bc
Vinicius Lazarini Pecchia <vinicius.pecchia@venturus.org.br>
2021-08-11 08:28:17 -0300

    [Dev]More Sections

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,77 +1,80 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
 
-
-function Section() {
+function Section({
+  title,
+  description,
+  leftBtnText,
+  rightBtnText,
+  backgroundImg,
+}) {
   return (
-    <Wrap>
-       <ItemText>
-          <h1>Model S</h1>
-          <p>Order Online for Touchless Delivery</p>
-       </ItemText>
-       <Buttons>
+    <Wrap bgImage={backgroundImg}>
+      <ItemText>
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </ItemText>
+      <Buttons>
         <ButtonGroup>
-          <LeftButton>
-              Custom Order
-          </LeftButton>
-          <RightButton>
-              Existing Inventory
-          </RightButton>
-       </ButtonGroup>
-        <DownArrow src="/images/down-arrow.svg"/>
-       </Buttons>
+          <LeftButton>{leftBtnText}</LeftButton>
+          <RightButton>{rightBtnText}</RightButton>
+        </ButtonGroup>
+        <DownArrow src="/images/down-arrow.svg" />
+      </Buttons>
     </Wrap>
-  )
+  );
 }
 
-export default Section
+export default Section;
 
 const Wrap = styled.div`
     width: 100vw;
     height: 100vh;
-    background-image: url('/images/model-3.jpg');
+    background-image: url('/images/model-s.jpg');
     background-position: center;
     background-repeat: no-repeat;
     background-size: cover;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-    align-items: center
-
-`
-const ItemText = styled.div`
-    padding-top: 15vh;
-    text-align: center;
-`
-const ButtonGroup = styled.div`
-    display: flex;
-    margin-bottom: 30px;
-`
-const LeftButton = styled.div`
-    background-color: rgba(23, 26, 32, 0.8);
-    height: 40px;
-    width: 256px;
-    color: white;
-    display: flex;
-    justify-content: center;
     align-items: center;
-    border-radius: 100px;
-    opacity: 0.85;
-    text-transform: uppercase;
-    font-size: 12px;
-    cursor: pointer;
-    margin: 8px;
-`
+    background-image:${props => `url("/images/${props.bgImage}")` };
+
+`;
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 8px;
+`;
 const RightButton = styled(LeftButton)`
-
-`
+  background: white;
+  opacity: 0.65;
+  color: black;
+`;
 const DownArrow = styled.img`
-    height: 40px;
-    overflow-x: hidden;
-    animation: animateDown infinite 1.5s;
-    
-`
-const Buttons = styled.div`
-    
-
-`
+  height: 40px;
+  overflow-x: hidden;
+  animation: animateDown infinite 1.5s;
+`;
+const Buttons = styled.div``;
